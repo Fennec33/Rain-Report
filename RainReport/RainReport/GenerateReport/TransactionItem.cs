6ca6db2719b018cda7cfef297a9f26b99fa3502b
Fennec33 <66853066+Fennec33@users.noreply.github.com>
2022-08-02 23:55:25 -0700

    made main report classes

---
+++ RainReport/RainReport/GenerateReport/TransactionItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RainReport.GenerateReport
+{
+    internal class TransactionItem
+    {
+    }
+}
