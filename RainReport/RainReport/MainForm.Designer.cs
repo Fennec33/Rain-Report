1bb01065237515c294e7019f14794f926c22345d
Fennec33 <66853066+Fennec33@users.noreply.github.com>
2022-08-03 00:14:10 -0700

    added form elements

--- RainReport/RainReport/MainForm.Designer.cs
+++ RainReport/RainReport/MainForm.Designer.cs
@@ -30,6 +30,9 @@ partial class MainForm
     private void InitializeComponent()
     {
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_RunReport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listBox1
@@ -43,19 +46,56 @@ partial class MainForm
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
             // 
-            // Form1
+            // btn_RunReport
+            // 
+            this.btn_RunReport.Location = new System.Drawing.Point(200, 297);
+            this.btn_RunReport.Name = "btn_RunReport";
+            this.btn_RunReport.Size = new System.Drawing.Size(116, 42);
+            this.btn_RunReport.TabIndex = 1;
+            this.btn_RunReport.Text = "Run Report";
+            this.btn_RunReport.UseVisualStyleBackColor = true;
+            this.btn_RunReport.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 297);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "End Of Day Report";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 324);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Transaction Details";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(350, 364);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_RunReport);
             this.Controls.Add(this.listBox1);
-            this.Name = "Form1";
+            this.Name = "MainForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
     #endregion
 
     private ListBox listBox1;
+    private Button btn_RunReport;
+    private Label label1;
+    private Label label2;
 }
